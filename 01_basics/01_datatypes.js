d3f0c8e71e3f0cc1ed952d65f4121e27bb6710dc
C0DEFS2025 <code.full.stack.2025@gmail.com>
2025-01-11 23:25:38 +0000

    variable and data types

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,10 @@
+"use strict"; // treat all code as a newer version
+
+//number=> 2 to power 53
+//name="aman" string
+//boolean="true/false"
+//null=>standalone value
+//symbol=>unique
+
+
+console.log(typeof Boolean)
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountid=21221
+let accountemail="code.2025@gmail.com"
+let accountpassword="231234"
+let accountcity="jaipur"
+
+console.log([accountid,accountemail,accountpassword,accountcity])
